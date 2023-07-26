7d1ed6f07e9b15ce860cccc3e404959a495444e3
majun <2113725201@qq.com>
2023-07-26 14:37:28 +0800

    将点云数据上传到mysql数据库

--- src/main/java/com/geosot/javademo/mysql/TxtFileToDatabase.java
+++ src/main/java/com/geosot/javademo/mysql/TxtFileToDatabase.java
@@ -1,30 +1,28 @@
 package com.geosot.javademo.mysql;
 
+import com.geosot.javademo.units.MySQLConnect;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class TxtFileToDatabase {
 
     public static void main(String[] args) {
-        String filePath = "path/to/your_file.txt"; // 替换为你的txt文件路径
-        String dbUrl = "jdbc:mysql://localhost:3306/your_database_name"; // 替换为你的数据库连接URL
-        String username = "your_username"; // 替换为你的数据库用户名
-        String password = "your_password"; // 替换为你的数据库密码
-
+        String filePath = "D:\\JavaConsist\\MapData\\180m_pointcloud\\0\\new\\part-all-clean.txt";
+        long start = System.currentTimeMillis();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath));
-             Connection connection = DriverManager.getConnection(dbUrl, username, password)) {
+             Connection connection = MySQLConnect.getConnection()) {
 
-            String insertQuery = "INSERT INTO your_table_name (code, x, y, z, lat, lon) VALUES (?, ?, ?, ?, ?, ?)";
+            String insertQuery = "INSERT INTO las_0 (code, x, y, z, lat, lon) VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
 
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] data = line.split("\\s+"); // 假设数据以空白字符分隔
+                String[] data = line.split(" "); // 假设数据以空白字符分隔
                 if (data.length == 6) {
                     String code = data[5];
                     double x = Double.parseDouble(data[0]);
@@ -42,16 +40,17 @@ public class TxtFileToDatabase {
 
                     preparedStatement.executeUpdate();
                 } else {
-                    System.out.println("Invalid data format: " + line);
+                    System.out.println("数据格式无效: " + line);
                 }
             }
-
-            System.out.println("Data insertion complete!");
+            connection.commit();
+            System.out.println((System.currentTimeMillis() - start)+"ms");
+            System.out.println("插入数据完成");
 
         } catch (IOException e) {
-            System.out.println("Error reading the file: " + e.getMessage());
+            System.out.println("文件读取错误: " + e.getMessage());
         } catch (SQLException e) {
-            System.out.println("Error connecting to the database or executing query: " + e.getMessage());
+            System.out.println("连接数据库失败: " + e.getMessage());
         }
     }
 }
