03eedfc10b4ef2a9a5ef661a9430d62b884dedf5
majun <2113725201@qq.com>
2023-08-08 23:51:27 +0800

    leetcode day two

--- src/main/java/com/geosot/javademo/leetcode/LeetCodeDayTwo.java
+++ src/main/java/com/geosot/javademo/leetcode/LeetCodeDayTwo.java
@@ -33,4 +33,43 @@ public class LeetCodeDayTwo {
         // 若res与原始值相等则表示整个数组加起来都没有大于target，否则就返回最小的子数组长度
         return res==Integer.MIN_VALUE?0:res;
     }
+
+    /**
+     * 给你一个正整数 n ，生成一个包含 1 到 n*n 所有元素
+     * 且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix。
+     * @param n 正整数 n
+     * @return 返回螺旋数组
+     */
+    public int[][] generateMatrix(int n) {
+        int left = 0, right = n-1, top = 0, bottom = n-1;
+        int count = 1, target = n * n;
+        int[][] res = new int[n][n];
+        //for循环中变量定义成i或j的细节：按照通常的思维，i代表行，j代表列
+        //这样，就可以很容易区分出来变化的量应该放在[][]的第一个还是第二个
+        //对于变量的边界怎么定义：
+        //从左向右填充：填充的列肯定在[left,right]区间
+        //从上向下填充：填充的行肯定在[top,bottom]区间
+        //从右向左填充：填充的列肯定在[right,left]区间
+        //从下向上填充：填充的行肯定在[bootom,top]区间
+        //通过上面的总结会发现边界的起始和结束与方向是对应的
+        while(count <= target){
+            //从左到右填充，相当于缩小上边界
+            for(int j = left; j <= right; j++) res[top][j] = count++;
+            //缩小上边界
+            top++;
+            //从上向下填充，相当于缩小右边界
+            for(int i = top; i <=bottom; i++) res[i][right] = count++;
+            //缩小右边界
+            right--;
+            //从右向左填充，相当于缩小下边界
+            for(int j = right; j >= left; j--) res[bottom][j] = count++;
+            //缩小下边界
+            bottom--;
+            //从下向上填充，相当于缩小左边界
+            for(int i = bottom; i >= top; i--) res[i][left] = count++;
+            //缩小左边界
+            left++;
+        }
+        return res;
+    }
 }
